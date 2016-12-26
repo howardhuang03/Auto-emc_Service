a2d4d3c43f8e3737559a3c0a379998a6198240aa
Howardhuang <howardhuang03@gmail.com>
2016-12-26 21:30:01 +0800

    Add a client for ThingSpeak test
    Signed-off-by: Howardhuang <howardhuang03@gmail.com>

--- main.go
+++ main.go
@@ -58,11 +58,11 @@ func httpHnadler (fname string) {
   http.ListenAndServe("localhost:3000", r)
 }
 
-func mqttHandler() {
+func mqttLocalHandler() {
   //create a ClientOptions struct setting the broker address, clientid, turn
   //off trace output and set the default message handler
   opts := MQTT.NewClientOptions().AddBroker("tcp://127.0.0.1:1883")
-  opts.SetClientID("go-simple")
+  opts.SetClientID("go-local")
   opts.SetDefaultPublishHandler(f)
 
   //create and start a client using the above ClientOptions
@@ -79,6 +79,31 @@ func mqttHandler() {
   }
 }
 
+func mqttCloudHandler() {
+  id := "XXXXX"
+  key := "XXXXX"
+  topic := fmt.Sprintf("channels/%s/publish/%s", id, key)
+
+  opts := MQTT.NewClientOptions().AddBroker("tcp://mqtt.thingspeak.com:1883")
+  opts.SetClientID("go-cloud")
+  opts.SetDefaultPublishHandler(f)
+
+  c := MQTT.NewClient(opts)
+  if token := c.Connect(); token.Wait() && token.Error() != nil {
+    panic(token.Error())
+  }
+
+  //Publish 5 messages to ThingSpeak at qos 0 and wait for the receipt
+  //from the server after sending each message
+  for i := 0; i < 5; i++ {
+    text := fmt.Sprintf("field1=%d&field2=%d", i, i + 10)
+    token := c.Publish(topic, 0, false, text)
+    fmt.Printf("TOPIC: %s, MSG: %s\n", topic, text)
+    token.Wait()
+    time.Sleep(20 * time.Second)
+  }
+}
+
 func main() {
   fname := fmt.Sprintf("%s.csv", filePrefix())
   var err error
@@ -86,7 +111,7 @@ func main() {
   check(err)
 
   go httpHnadler(fname)
-  go mqttHandler()
+  go mqttCloudHandler()
 
   for {
   }
